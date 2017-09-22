52524f594ba7bde57eca6468136387a5db3ae4c9
Raquel <Raquel@air-de-raquel.home>
2017-09-23 00:16:34 +0200

    Sincronizar travis y sonar

--- scripts/sonar.sh
+++ scripts/sonar.sh
@@ -1,5 +1,5 @@
 #echo off
-export workspace=/Users/Raquel/Documents/Workspace/Eclipse/Raquel.Serrano.SVC.miw.upm.es.aux
+export workspace=/Users/Raquel/Documents/Workspace/Eclipse/Raquel.Serrano.SVC.miw.upm.es
 export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_144.jdk/Contents/Home/jre
 export M2_HOME=/Users/Raquel/Desktop/Master/Software/apache-maven-3.5.0
 
@@ -23,4 +23,4 @@ echo . Se analiza y sube a sonar cloud
 mvn sonar:sonar \
   -Dsonar.organization=raquelsr-github \
   -Dsonar.host.url=https://sonarcloud.io \
-  -Dsonar.login=d9f33c86d76eb0827a41917e1708a8a446c1ccb4 --settings settings.xml
+  -Dsonar.login=61cf32b8ff7a3e271a2099fbac28be6789110311 --settings settings.xml
