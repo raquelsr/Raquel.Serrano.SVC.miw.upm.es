f38569da6efab829210cb77aa5fc02eba4d49742
Raquel <Raquel@air-de-raquel.home>
2017-09-23 00:28:36 +0200

    Create TestC32

---
+++ src/test/java/es/upm/miw/svc/raquel/C32Test.java
@@ -0,0 +1,22 @@
+package es.upm.miw.svc.raquel;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class C32Test {
+
+    C32 c32;
+    
+    @Before
+    public void before() {
+        c32 = new C32();
+    }
+    
+    @Test
+    public void testMA() {
+        assertEquals("mA", c32.mA());
+    }
+
+}
