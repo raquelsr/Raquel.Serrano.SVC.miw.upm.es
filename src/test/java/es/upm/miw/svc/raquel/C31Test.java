1161a4129ce7562d536a0f563afde88d38079774
Raquel <Raquel@air-de-raquel.home>
2017-09-23 00:26:51 +0200

    Create TestC31

---
+++ src/test/java/es/upm/miw/svc/raquel/C31Test.java
@@ -0,0 +1,26 @@
+package es.upm.miw.svc.raquel;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class C31Test {
+
+    private C31 c31;
+    
+    @Before
+    public void before() {
+        c31 = new C31();
+    }
+
+    @Test
+    public void testM1() {
+       assertEquals("m1", c31.m1());
+    }
+
+    @Test
+    public void testM2() {
+        assertEquals("m2", c31.m2());
+    }
+}
