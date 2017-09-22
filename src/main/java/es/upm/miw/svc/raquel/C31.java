f200408b98ab27200f9f2f1d368a3997f6e95720
Raquel <Raquel@air-de-raquel.home>
2017-09-23 00:25:50 +0200

    Create Class C31

---
+++ src/main/java/es/upm/miw/svc/raquel/C31.java
@@ -0,0 +1,19 @@
+package es.upm.miw.svc.raquel;
+
+public class C31 {
+
+    public C31() {
+        //Comentario
+    }
+    
+    private String m1 = "m1";
+    private String m2 = "m2";
+    
+    public String m1() {
+        return m1;
+    }
+    
+    public String m2() {
+        return m2;
+    }
+}
